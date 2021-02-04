3c82b293459174e7c2127b54567ab69623b50f58
Giacomo Mariani <giacomo.mariani@aalto.fi>
2021-02-04 07:42:44 +0200

    added binary heap exercise

--- content.js
+++ content.js
@@ -17,6 +17,43 @@ const Content = {
     </div>
     `,
     exercise_js: `insertionSort.js`
+  },
+  heap_build: {
+    title: `Heap Build`,
+    head: `
+    <link rel="stylesheet" href="/static/jsav-vas/style/heapPRO.css" type="text/css" />
+    <script src="/static/jsav-vas/js/binaryheap.js"></script>
+    `,
+    body: `
+    <div id="container">
+      <table class="proHeaderTable">
+        <tr>
+          <td class="headerLeft"></td>
+          <td class="headerCenter">
+            <p class="jsavexercisecontrols"></p>
+          </td>
+        </tr>
+      </table>
+      <form class="avcontainer">
+        <h1>Heap Build Proficiency Exercise</h1>
+        <p class="instructLabel">Instructions</p>
+        <p class="instructions">
+            In this exercise, your task is to run the build-heap
+            algorithm on the heap. The min-heap is visualized both as
+            a binary tree and array and the exercise can be completed
+            by swapping keys in either visualization. You can swap two
+            elements by first clicking on one element and then
+            another.
+        </p>
+        <p style="text-align: center">
+          <span class="jsavscore"></span>
+        </p>
+        <div id="av" class="jsavcanvas">
+        </div>
+      </form>
+    </div>
+    `,
+    exercise_js: `heapBuildPRO.js`
   }
 }
 
